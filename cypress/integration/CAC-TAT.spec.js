47cc1936e9d3d0993b61cb7fcbdcf14780fbaee5
Gabriel Duarte <grd1996@gmail.com>
2023-05-05 12:18:53 -0300

    resolved_exercise_2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,7 +9,33 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('preenche os campos obrigatórios e envia o formulário', function () {
-        cy.get('firstName')
+        cy.get('#firstName')
+            .should('be.visible')
             .type('Gabriel')
+            .should('have.value', 'Gabriel')
+
+            cy.get('#lastName')
+            .should('be.visible')
+            .type('Duarte')
+            .should('have.value', 'Duarte')
+
+            cy.get('#email')
+            .should('be.visible')
+            .type('gabrielzito@qa.com.br')
+            .should('have.value', 'gabrielzito@qa.com.br')
+
+            cy.get('#open-text-area')
+            .should('be.visible')
+            .type('Socorro meu deus do céu me ajuda pelo amor de deus', {delay: 0} )
+            .should('have.value', 'Socorro meu deus do céu me ajuda pelo amor de deus')
+
+            cy.get('.button')
+            .should('be.visible')
+            .click()
+
+            cy.get('.success')
+            .should('be.visible')
     })
+
+
 })
